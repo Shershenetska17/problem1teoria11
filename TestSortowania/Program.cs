5f89fc5172c10f36fb6e549a65a6f44d3c7ecdd7
shers <shers@LIZA>
2024-05-07 20:19:55 +0200

    Add project files.

---
+++ TestSortowania/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TestSortowania;
+using Sortowanie;
+internal class Program
+{
+
+    static void Main(string[] args)
+    {
+        int[] liczby = new int[10];
+        Random r = new Random();
+        Console.WriteLine("Tablica przed sortowaniem:");
+        for (int i = 0; i < liczby.Length; i++)
+        {
+            liczby[i] = r.Next(0, 1001);
+            Console.WriteLine("{0}", liczby[i]);
+        }
+        Sortowanie.Sortuj(liczby);
+
+        Console.WriteLine("\n\nTablica po sortowaniu:");
+        foreach (int i in liczby)
+        {
+            Console.WriteLine("{0}, ", i);
+        }
+
+        Console.ReadKey();
+    }
+}
